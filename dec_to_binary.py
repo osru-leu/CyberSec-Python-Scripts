345e293d2ee426495fc04dfc872b93a810f7d42b
osru-leu <urso.samuel.p@gmail.com>
2023-06-07 21:22:27 -0500

    Update

--- dec_to_binary.py
+++ dec_to_binary.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+# Converts a decimal e.g. 97, to its binary value. Shortend version of decimal_to_binary.py
+# which has a line by line explanation of what the code is doing. This was an early assignment in Flatiron School   
+
 #mynumber = 97
 output = ""
 myvalue = 97
