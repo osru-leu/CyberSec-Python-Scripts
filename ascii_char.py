4b1c6023b94532ac1e1b864fb768664bf578de15
osru-leu <urso.samuel.p@gmail.com>
2023-06-07 21:16:29 -0500

    Update

--- ascii_char.py
+++ ascii_char.py
@@ -1,8 +1,10 @@
 
+# The first for loop below ouputs A-Z and a-z with their corresponding ascii number
+# The second loop only outputs a-z (lowercase) with their corresponding ascii number
 
 for num, let in zip(range(65, 91), range(97, 123)):
     print(num, chr(num), let, chr(let))
     
-#for let in range(97, 123):
-    #print(let, chr(let))
-#print(num, chr(num), let, chr(let))
+for let in range(97, 123):
+    print(let, chr(let))
+print(num, chr(num), let, chr(let))
