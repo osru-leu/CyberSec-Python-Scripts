38135bb02eee82f5fe551f4604f0b629e42202e4
osru-leu <urso.samuel.p@gmail.com>
2023-07-30 14:23:50 -0500

    Update

--- encode_decode_base64.py
+++ encode_decode_base64.py
@@ -1,12 +1,13 @@
 
 import base64
+import pickle
 
 base64_str = "VEhNe0JBU0U2NF9FTkNPRElOR30=" # Replace with string to decode
 decoded_bytes = base64.b64decode(base64_str)
 print(decoded_bytes)
 
 # Encode using base64
-message = '"id":1, "admin":True' # Replace with your mess to encode
+message = "THM{BASE64_ENCODING}" # Replace with your message to encode
 message_bytes = message.encode('ascii')
 base64_bytes = base64.b64encode(message_bytes)
 base64_message = base64_bytes.decode('ascii')
